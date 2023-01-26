6cdfd75d66d2dbd9b7b6f4955816d0fb8f84ef92
Verth <vertisixs@gmail.com>
2023-01-27 00:36:42 +0100

    Fixing constructor
    Arrays are Mutable Objects which  means that in the there will be many variables referencing the same Object which allows unwanted file modification

--- section seven/Main.java
+++ section seven/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car("Nissan", 10000, 2020, "Green");
-        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
+        String[] spareParts = new String[] { "Tires", "Keys" };
+
+        Car nissan = new Car("Nissan", 10000, 2020, "Green", spareParts);
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue", spareParts);
+
+        spareParts[0] = "Filter";
 
         Car nissan2 = new Car(nissan);
         nissan2.setColor("Yellow");
