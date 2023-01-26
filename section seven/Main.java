9f7ea1f2d232ae0dbe9cb409cacae602f3513c12
Verth <vertisixs@gmail.com>
2023-01-26 23:54:16 +0100

    section seven

---
+++ section seven/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Car nissan = new Car("Nissan", 10000, 2020, "Green");
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
+
+        Car nissan2 = new Car(nissan);
+        nissan2.setColor("Yellow");
+
+    }
+}
