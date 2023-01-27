de8593e546c98bb8e7e590997046833db2393c8a
Verth <vertisixs@gmail.com>
2023-01-27 01:04:27 +0100

    Workbook 7.6

--- section seven/Main.java
+++ section seven/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Car dodge = new Car("Dodge", 11000, 2019, "Blue", new String[] { "Tires", "Keys" });
 
         Car nissan2 = new Car(nissan);
+        
 
         nissan2.setColor("Yellow");
         nissan2.drive();
