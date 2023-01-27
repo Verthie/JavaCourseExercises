51dc5b920ef8597b5d1de5736b04ce43bce48c90
Verth <vertisixs@gmail.com>
2023-01-27 01:38:54 +0100

    Another reference trap while assigning objects
    Overcoming the reference trap while assigning car objects to dealership by assigning them in a constructor one by one with the use of a loop.

--- section seven/Main.java
+++ section seven/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car nissan = new Car("Nissan", 10000, 2020, "Green", new String[] { "Tires", "Keys" });
-        Car dodge = new Car("Dodge", 11000, 2019, "Blue", new String[] { "Tires", "Keys" });
-        Car nissan2 = new Car(nissan);
+        Car[] cars = new Car[] {
+                new Car("Nissan", 5000, 2020, "red", new String[] { "tires", "keys" }),
+                new Car("Dodge", 8500, 2019, "blue", new String[] { "tires", "keys" }),
+                new Car("Nissan", 5000, 2017, "yellow", new String[] { "tires", "filter" }),
+                new Car("Honda", 7000, 2019, "orange", new String[] { "tires", "filter" }),
+                new Car("Mercedes", 12000, 2015, "jet black", new String[] { "tires", "filter", "transmission" })
+        };
 
-        System.out.println(nissan);
+        Dealership dealership = new Dealership(cars);
 
-        nissan2.setColor("Yellow");
-        nissan2.drive();
+        cars[3].setColor("Blue");
     }
 }
